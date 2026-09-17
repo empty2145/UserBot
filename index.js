f085e535f578e082dfbbaa51349d228a5d08a2c7
empty2145 <tasybaevtamir@gmail.com>
2026-09-17 23:41:53 +0500

    install: libraries

---
+++ index.js
@@ -0,0 +1,8 @@
+const { TelegramClient } = require("telegram");
+const { StringSession } = require("telegram/sessions");
+const input = require("input");
+
+const apiID;
+const apiHash;
+const StringSession = new StringSession("");
+
