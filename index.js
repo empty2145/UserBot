e634c25e21b061e69481212e3836ee8689ef2d74
empty2145 <tasybaevtamir@gmail.com>
2026-09-17 23:46:20 +0500

    get client

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+require("dotenv").config();
 const { TelegramClient } = require("telegram");
 const { StringSession } = require("telegram/sessions");
 const input = require("input");
 
-const apiID;
-const apiHash;
-const StringSession = new StringSession("");
+const apiID = parseInt(process.env.API_ID);
+const apiHash = process.env.API_HASH;
+const StringSession = new StringSession(process.env.STRING_SESSION || "");
+
+const client = new TelegramClient(StringSession, apiID, apiHash, {
+    connectionRetries: 5,
+});
 
