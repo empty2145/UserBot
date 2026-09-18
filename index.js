75e4bf10ab49ead65f9175bbeff1cdc3a8118c1e
empty2145 <tasybaevtamir@gmail.com>
2026-09-18 23:08:13 +0500

    refactor: some code for automatization

--- index.js
+++ index.js
@@ -5,36 +5,41 @@ const input = require("input");
 
 const apiID = parseInt(process.env.API_ID);
 const apiHash = process.env.API_HASH;
-const stringSession = new StringSession(process.env.STRING_SESSION || "");
+const stringSession = new StringSession(process.env.STRING_SESSION);
 
 const client = new TelegramClient(stringSession, apiID, apiHash, {
     connectionRetries: 5,
 });
 
+function randomizer() {
+  const foods = [
+    "",
+    "",
+    "",
+  ]
+
+  
+}
+
 (async () => {
     console.log("Authentificating...");
 
-    await client.start({
-        phoneNumber: async () => await input.text("Number: "),
-        password: async () => await input.text("Password: "),
-        phoneCode: async () => await input.text("Code from Telegram: "),
-        onError: (err) => console.log(err),
-    })
+    await client.start({})
 
     console.log("Connected!");
 
-    if (!process.env.STRING_SESSION) {
-    console.log("\n=== ACTION REQUIRED ===");
-    console.log("Copy the string below and paste it as your STRING_SESSION in the .env file:\n");
-    console.log(client.session.save());
-    console.log("\nThen restart the script.");
-    process.exit(0);
-  }
-  await client.sendMessage("rig_test_01", {
-    message: "Hi everyone! I'm offering IELTS and SAT tutoring. DM me for details!",
-  });
+    const adMessage;
+    console.log(`Prepared Ad: "${adMessage}"`)
+
   
-  console.log("Message sent!");
+  try {
+    await client.sendMessage("rig_test_01", {
+      message: adMessage,
+    });
+    console.log("Message sent!");
+  } catch (error) {
+    console.error("Error sending message:", error);
+  }
   process.exit(0);
 })();
 
